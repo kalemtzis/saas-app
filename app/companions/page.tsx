70da8b22fb359e490c0d9526174900f3e395cada
kalemtzis <kalemtzis@csd.auth.gr>
2025-07-28 17:00:20 +0300

    add plan permissions | profile page

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,44 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
+import { PlusIcon } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+
+        <div className='flex gap-4'>
+          <SearchInput />
+
+          <SubjectFilter />
+
+          <Link href='/companions/new'>
+            <button className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit cursor-pointer bg-primary text-white'>
+              Companion
+              <PlusIcon size={15} />
+            </button>
+          </Link>
+        </div>
+      </section>   
+
+      <section className='companions-grid'>
+          {companions.map(companion => (
+            <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+          ))}
+      </section>
+    </main>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,33 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
-import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 })
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <h1 className='text-3xl'>Dashboard</h1>
 
       <section className='home-section'>
-        <CompanionCard 
-          id='1'
-          name='Neura the Brainy Explorer'
-          topic='Neural Network of the Brain'
-          subject='Science'
-          duration={45}
-          color='#E5D0FF'
-        />
-        <CompanionCard 
-          id='2'
-          name='Countsy the Number Wizard'
-          topic='Derivatives & Integrals'
-          subject='Maths'
-          duration={30}
-          color='#FFDA6E'
-        />
-        <CompanionCard 
-          id='1'
-          name='Verba the Vocabulary Builder'
-          topic='English Literature '
-          subject='Language'
-          duration={30}
-          color='#BDE7FF'
-        />
+        {companions.map(companion => (
+          <CompanionCard 
+            {...companion}
+            key={companion.id}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className='home-section'>
+
         <CompanionsList 
           title='Recently completed sessions'
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames='w-2/3 max-lg:w-full'
         />
         <CTA />
